99a562b71d70ce7304a6d1ddc15d1e1bba76ac7d
Bellaabdelouahab <Abdobella977@gmail.com>
2022-01-02 11:38:49 +0100

    315/

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from flask import Flask,render_template,redirect,url_for
+from flask import Flask,render_template,redirect,url_for,request
 from pymongo import MongoClient
 
 app = Flask(__name__)
 myclient = MongoClient('localhost', 27017) 
 my_database = myclient["Book"]  
 my_collection = my_database["Book"] 
+BooksToReserve = my_database["ReservedBooks"]
 
 
 @app.route("/")
 def home_page():
-    
-    
     # number of documents in the collection
     mydoc = my_collection.find()
     x=0
@@ -20,15 +19,48 @@ def home_page():
     a=0
     for y in reserved:
         a+=1
-    return render_template("DashBoard.html" , total = x , reserved = a)
+    c=0
+    books = BooksToReserve.find({})
+    for y in books:
+        c+=1
+    return render_template("DashBoard.html" , total = x , reserved = a,ToReserve=c)
 
-@app.route("/ToReserve")
+@app.route("/ToReserve",methods=["GET","POST"])
 def Customers():
-    return render_template("Reserve.html")
+    if request.method == "POST":
+        if request.form.get("Cancelation_i"):
+            BooksToReserve.delete_one({"Full Name":request.form.get("Cancelation_i")})
+        if request.form.get("Validation_i"):
+            i=BooksToReserve.find({"Full Name":request.form.get("Validation_i")})
+            Book={}
+            for y in i:
+                for key, value in y.items():
+                    Book=value
+            print(Book)
+            my_collection.update_one({"title":Book},{"$set":{"reserved":2}})
+            BooksToReserve.delete_one({"Full Name":request.form.get("Validation_i")})
+    books = BooksToReserve.find({})
+    y=0
+    BooksRes=[]
+    for i in books:
+        y=[]
+        for key, value in i.items():
+            y.append(value)
+        BooksRes.append(y)
+    return render_template("Reserve.html",ToReserve=BooksRes)
+    
 
 @app.route("/Books")
 def Books():
-    return render_template("Books.html")
+    books = my_collection.find({},{"title":1,"authors":1,"published_year":1,"_id":0})
+    y=0
+    Books=[]
+    for i in books:
+        y=[]
+        for key, value in i.items():
+            y.append(value)
+        Books.append(y)
+    return render_template("Books.html",Books=Books)
 
 if __name__ == "__main__":
     app.run(debug = True)
